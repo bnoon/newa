1b14f23edc11c0f0a004a0e2c9a84b106653df4a
Keith Eggleston <kle1@cornell.edu>
2016-03-03 10:31:45 -0500

    Updated splitStationLists to include inactive

--- newaCommon/splitStationInfoList.py
+++ newaCommon/splitStationInfoList.py
@@ -20,14 +20,39 @@ try:
 		outfil.write('stn_info = {')
 		for stn in state_dicts[state].keys():
 			outfil.write("'%s': %s,\n" % (stn, state_dicts[state][stn]))
-##			state_dicts[state][stn]['name'] = '%s, %s' %(state_dicts[state][stn]['name'], state_dicts[state][stn]['state'])
 			outfil_all.write("'%s': %s,\n" % (stn, state_dicts[state][stn]))
 		outfil.write('}')
 		outfil.close()	
-
 	outfil_all.write('}')
 	outfil_all.close()	
+except:
+	print "Error encountered"
+	print_exception()
 
+try:
+	from stn_info_inactive import stn_info_inactive
+	state_dicts = {}
+	for stn in stn_info_inactive.keys():
+		state = stn_info_inactive[stn]['state']
+		stn_info_inactive[stn]['name'] = stn_info_inactive[stn]['name'].replace(', %s' % state.upper(), '')
+		if not state_dicts.has_key(state):
+			state_dicts[state] = {}
+		state_dicts[state][stn] = stn_info_inactive[stn]
+			
+	outfil_all = open('stn_info_all_inactive.py', 'w')
+	outfil_all.write('stn_info = {')
+
+	for state in state_dicts.keys():
+		print 'processing inactive:',state
+#		outfil = open('stn_info_%s_inactive.py' % state.lower(), 'w')
+#		outfil.write('stn_info = {')
+		for stn in state_dicts[state].keys():
+#			outfil.write("'%s': %s,\n" % (stn, state_dicts[state][stn]))
+			outfil_all.write("'%s': %s,\n" % (stn, state_dicts[state][stn]))
+#		outfil.write('}')
+#		outfil.close()	
+	outfil_all.write('}')
+	outfil_all.close()	
 except:
 	print "Error encountered"
 	print_exception()
