c6078dfa0b15ebc7f42c31e3dd5b24f12b0427af
Keith Eggleston <kle1@cornell.edu>
2017-04-06 08:48:21 -0400

    add ky_ver, nc_gre; reactivate gra

--- newaCommon/stn_info_ky.py
+++ newaCommon/stn_info_ky.py
@@ -1,3 +1,4 @@
-stn_info = {'kfft': {'srqual': 'ok', 'state': 'KY', 'name': 'Frankfort', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 38.1847, 'elev': 804, 'lon': -84.9033, 'network': 'icao'},
+stn_info = {'ky_ver': {'name': 'Versailles', 'vars': ['temp', 'lwet', 'prcp', 'rhum', 'wspd', 'wdir', 'srad'], 'lon': -84.699025, 'state': 'KY', 'elev': 950, 'lat': 37.999985, 'network': 'newa'},
+'kfft': {'srqual': 'ok', 'state': 'KY', 'name': 'Frankfort', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 38.1847, 'elev': 804, 'lon': -84.9033, 'network': 'icao'},
 'klex': {'srqual': 'ok', 'state': 'KY', 'name': 'Lexington', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 38.0408, 'elev': 980, 'lon': -84.6058, 'network': 'icao'},
 }
