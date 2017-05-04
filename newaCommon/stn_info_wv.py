11a671868746acc4999fe3729cb40aedd0d25ed2
KEggleston <kle1@cornell.edu>
2017-05-04 07:58:24 -0400

    Delete stn_info_wv.py

--- newaCommon/stn_info_wv.py
+++
@@ -1,2 +0,0 @@
-stn_info = {'kmrb': {'srqual': 'ok', 'state': 'WV', 'name': 'Martinsburg', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 39.4019, 'elev': 534, 'lon': -77.9844, 'network': 'icao'},
-}
