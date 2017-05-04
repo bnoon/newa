576f0b7d70bbfe5cb459729e7b5cb0ce519a339d
KEggleston <kle1@cornell.edu>
2017-05-04 07:59:29 -0400

    Delete stn_info_de.py

--- newaCommon/stn_info_de.py
+++
@@ -1,3 +0,0 @@
-stn_info = {'kged': {'srqual': 'ok', 'state': 'DE', 'name': 'Georgetown', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 38.6892, 'elev': 51, 'lon': -75.3592, 'network': 'icao'},
-'kilg': {'srqual': 'ok', 'state': 'DE', 'name': 'Wilmington', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 39.67, 'elev': 79, 'lon': -75.6, 'network': 'icao'},
-}
