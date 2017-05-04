ffed919f2e52853f3d9ebe3c1ed904cc3adaa071
KEggleston <kle1@cornell.edu>
2017-05-04 07:58:45 -0400

    Delete stn_info_sc.py

--- newaCommon/stn_info_sc.py
+++
@@ -1,2 +0,0 @@
-stn_info = {'kgsp': {'srqual': 'ok', 'state': 'SC', 'name': 'Greer', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 34.8842, 'elev': 943, 'lon': -82.2208, 'network': 'icao'},
-}
