35df9a995a3e0fa7b21c54dbeb6754cc7aea1fbe
KEggleston <kle1@cornell.edu>
2017-05-04 07:58:39 -0400

    Delete stn_info_sd.py

--- newaCommon/stn_info_sd.py
+++
@@ -1,2 +0,0 @@
-stn_info = {'kfsd': {'srqual': 'ok', 'state': 'SD', 'name': 'Sioux Falls', 'vars': ['temp', 'prcp', 'rhum', 'wspd', 'wdir'], 'lat': 43.5778, 'elev': 1428, 'lon': -96.7539, 'network': 'icao'},
-}
